44d921f1afe3dc12e59ff50f22e86994ecdfdc6d
Jonathan <jonathanadcz@gmail.com>
2023-08-21 23:51:50 -0300

    Feat: Mongo Connection

--- desenvolvimento/src/main.py
+++ desenvolvimento/src/main.py
@@ -1,13 +1,42 @@
 from flask import Flask, jsonify
+from pymongo import MongoClient
+from pymongo import errors as pymongo_errors
+from utils.mongo_connection import MongoDBConnection
 import redis
 import base64
 import json
+
 
 # Criar uma instância do Flask
 app = Flask(__name__)
 
 # Crie uma conexão com o servidor Redis
 r = redis.Redis(host='redis', port=6379)
+clientMongo = MongoClient('mongodb://localhost:27017/')
+
+
+# Configurações do MongoDB
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+DB_NAME = 'trafegus'  # Nome do banco de dados
+ 
+
+mongo_connection = MongoDBConnection(MONGO_HOST, MONGO_PORT)
+
+# Rota para buscar dados na coleção 'cargas'
+@app.route('/get_cargas')
+def get_cargas():
+    try:
+        db = mongo_connection.get_database(DB_NAME)
+        collection = db.cargas  # Nome da coleção 'cargas'
+        data = list(collection.find())  # Recupera todos os documentos da coleção 'cargas'
+        return jsonify(data)
+    except pymongo_errors.ConnectionError as e:
+        error_message = "Error connecting to MongoDB: " + str(e)
+        print(error_message)
+        return jsonify({"error": error_message})
+
+
 
 
 # Rota para a página inicial
